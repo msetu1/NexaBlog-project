28c2c237d1690e650f8d2d6dee7777823080a012
msetu1 <msetu5763@gmail.com>
2025-02-23 20:34:02 +0600

    blog details page browser show done

---
+++ src/app/blogs/[blogId]/page.tsx
@@ -0,0 +1,14 @@
+import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
+
+const BlogDetails = async({params}:{params:Promise<{blogId:string}>}) => {
+        const {blogId}=await params;
+        const res =await fetch(`http://localhost:5000/blogs/${blogId}`)
+        const blog=await res.json();
+    return (
+        <div className="my-10">
+            <BlogDetailsCard blog={blog} />
+        </div>
+    );
+};
+
+export default BlogDetails;
